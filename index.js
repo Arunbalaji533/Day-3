ac5d6adf46863366f6b3fc841b33d4a814acd6f6
Arunbalaji533 <115739814+Arunbalaji533@users.noreply.github.com>
2022-10-19 12:08:18 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var resume =[{
+ 
+    "name":"arunbalaji",
+    "work": "student",
+    },
+    {
+    "qualification":"b.tech",
+    "place": "india",
+    "age":"23",
+    },
+    {
+      "skills":" kabadi,shettle,vollyball,cricket",
+      "experience": "fresher",
+      "languages" : "english ,tamil, hindi theriyathu poda",
+    }, 
+
+     {
+       "technical skills":"pursuing full stack develepment course"
+     
+   
+   }]
+   
+   console.log(resume)
+   
